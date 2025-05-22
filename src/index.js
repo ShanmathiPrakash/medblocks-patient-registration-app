8c9ebfa3249e246f4bddcf3d89edb91445d0bcd0
shanmathi <shanmathi@gmail.com>
2025-05-22 12:29:09 +0530

    SMS feature is added successfully, using the twilio third party messaging tool

--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+
+require('dotenv').config();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
