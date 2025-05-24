1547d64e08eeba725d876f17676896ed3ce66acd
shanmathi <shanmathi@gmail.com>
2025-05-24 14:22:26 +0530

    In the page sql, i have added view query tab and functionalities

--- src/db/dbInit.js
+++ src/db/dbInit.js
@@ -4,26 +4,20 @@ import { PGlite } from "https://cdn.jsdelivr.net/npm/@electric-sql/pglite/dist/i
 export const db = new PGlite("idb://patient-db");
 
 export async function initDatabase() {
-  await db.exec(`
-    CREATE TABLE IF NOT EXISTS patients (
-      id SERIAL PRIMARY KEY,
-      first_name TEXT NOT NULL,
-      last_name TEXT NOT NULL,
-      date_of_birth DATE NOT NULL,
-      gender TEXT NOT NULL,
-      email TEXT,
-      phone TEXT, 
-      address TEXT,
-      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
-  `);
-//If you want any columns need to be added use the below code: 
-  try {
-    await db.exec(`ALTER TABLE patients ADD COLUMN report TEXT;`);
-    console.log("Column 'report' added.");
-  } catch (err) {
-    if (!/duplicate column|already exists/i.test(err.message)) {
-      console.error("Error adding 'report' column:", err);
-    }
-  }
+  await db.query(`
+   CREATE TABLE IF NOT EXISTS patients (
+     id SERIAL PRIMARY KEY,
+        first_name TEXT NOT NULL,
+        last_name TEXT NOT NULL,
+        date_of_birth DATE NOT NULL,
+        gender TEXT NOT NULL,
+        email TEXT,
+        phone TEXT,
+        address TEXT,
+        image TEXT,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      );
+    `);
+
+  
 }
